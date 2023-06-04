e3ea58e2aa6173735115ba239fa3f70f3d62311f
AnnaVlasyk <annavlasik20@gmail.com>
2023-06-04 15:59:35 +0300

    Update fetchPicture.js

--- src/js/fetchPicture.js
+++ src/js/fetchPicture.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export async function fetchPhotos(name, page) {
+export async function fetchPicture(name, page) {
 	const API_KEY = '35180291-5eb2fdc93be8c0d6ffda745d8';
 	const API_URL = `https://pixabay.com/api/?key=35180291-5eb2fdc93be8c0d6ffda745d8&q=yellow+flowers&image_type=photo`
 
