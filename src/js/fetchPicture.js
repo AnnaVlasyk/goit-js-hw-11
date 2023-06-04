e37b1ca24871f7fa4b24c7d26586f3251ff12a6f
AnnaVlasyk <annavlasik20@gmail.com>
2023-06-04 15:54:12 +0300

    Update fetchPicture.js

--- src/js/fetchPicture.js
+++ src/js/fetchPicture.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function fetchPhotos(name, page) {
 	const API_KEY = '35180291-5eb2fdc93be8c0d6ffda745d8';
-	const API_URL = `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+	const API_URL = `https://pixabay.com/api/?key=35180291-5eb2fdc93be8c0d6ffda745d8&q=yellow+flowers&image_type=photo`
 
 	try {
 		const response = await axios.get(API_URL);
