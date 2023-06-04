db43f518de48085dcab7b4b728141b7f18826cf0
AnnaVlasyk <annavlasik20@gmail.com>
2023-06-04 16:26:30 +0300

    Update fetchPicture.js

--- src/js/fetchPicture.js
+++ src/js/fetchPicture.js
@@ -2,8 +2,13 @@ import axios from "axios";
 
 export async function fetchPicture(name, page) {
 	const API_KEY = '35180291-5eb2fdc93be8c0d6ffda745d8';
-	const API_URL = `https://pixabay.com/api/?key=35180291-5eb2fdc93be8c0d6ffda745d8&q=yellow+flowers&image_type=photo`
-
+	const URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+$.getJSON(URL, function(data){
+if (parseInt(data.totalHits) > 0)
+    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+else
+    console.log('No hits');
+});
 	try {
 		const response = await axios.get(API_URL);
 		return response;
@@ -11,5 +16,6 @@ export async function fetchPicture(name, page) {
 		console.error(error);
 	}
 }
+
 
 
